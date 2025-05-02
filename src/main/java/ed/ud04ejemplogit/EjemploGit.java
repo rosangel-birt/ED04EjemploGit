5a4ea64d996f72bfe9deeab24a7adab1d01d8c72
Rosangel <kryword@gmail.com>
2025-05-02 13:05:15 +0200

    Version 1. Commit con cambios básicos

--- src/main/java/ed/ud04ejemplogit/EjemploGit.java
+++ src/main/java/ed/ud04ejemplogit/EjemploGit.java
@@ -1,11 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ed.ud04ejemplogit;
 
 /**
- *
+ * Descripcion de la clase
  * @author krywo
  */
 public class EjemploGit {
@@ -13,6 +9,7 @@ public class EjemploGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    
+        
+        System.out.println("Ejmplo Git");
     }
 }
